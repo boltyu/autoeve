dc10f033a5cd84fdcee8ec2201f26ad29118227a
boltyu <lightingyu@gmail.com>
2021-02-04 21:11:40 +0800

    20210204

--- __main__.py
+++ __main__.py
@@ -15,7 +15,7 @@ try:
 except:
     shiptype = "venture"
     deviceindex = 0
-devicelist = ['127.0.0.1:5595','127.0.0.1:5645','192.168.1.6:5555','de496248']
+devicelist = ['127.0.0.1:5595','127.0.0.1:5645','192.168.1.5:5555','de496248']
 devicename = devicelist[deviceindex]
 print("Ship:",shiptype)
 print("Device:",devicename)
